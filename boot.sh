1bf1dafee31beda610a06bb66857c59875004971
Alex Kaplan <kaplan2539@gmail.com>
2018-07-12 23:03:23 -0700

    changed paths in boot.sh

--- boot.sh
+++ boot.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sunxi-fel -p uboot u-boot-sunxi-with-spl.bin \
-          write 0x43300000 rootfs.cpio.uboot \
-          write 0x43000000 sun5i-r8-chip.dtb \
-          write 0x43100000 u-boot.env \
-          write 0x42000000 zImage
-#sunxi-fel exec 0x4a000000
+IMAGES=buildroot/output/images
+
+sunxi-fel -p uboot $IMAGES/u-boot-sunxi-with-spl.bin \
+          write 0x43300000 $IMAGES/rootfs.cpio.uboot \
+          write 0x43000000 $IMAGES/sun5i-r8-chip.dtb \
+          write 0x42000000 $IMAGES/zImage \
+          write 0x43100000 buildroot_external/board/nextthing/chip/u-boot.env \
